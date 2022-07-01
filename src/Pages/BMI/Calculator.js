fefd102dcec44faaf518882c1141322928ea69f2
Marcin Garski <marcin.garski@interia.eu>
2022-06-30 20:44:52 -0400

    Toggle between genderBox

--- src/Pages/BMI/Calculator.js
+++ src/Pages/BMI/Calculator.js
@@ -1,10 +1,27 @@
 import React from "react";
 import style from './BMI.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleCheck, faMars, faVenus } from '@fortawesome/free-solid-svg-icons';
+import { faMars, faVenus } from '@fortawesome/free-solid-svg-icons';
 
 
 function Calculator() {
+
+  function removeSelected() {
+    const selectedBox = document.querySelectorAll('#head')
+    console.log(selectedBox.classList)
+    selectedBox.forEach(item => {
+      item.classList.remove(`${style.activeBox}`)
+    })
+  }
+
+  const handleToggle = (e) => {
+    if (!e.currentTarget.className.includes("activeBox")) {
+      removeSelected()
+      e.target.classList.add(`${style.activeBox}`)
+    } else {
+      e.target.classList.remove(`${style.activeBox}`)
+    }
+  };
   return (
     <>
       <div className={`${style.img}`}></div>
@@ -15,13 +32,12 @@ function Calculator() {
       <div className={`${style.middleSection}`}>
         <h3 className={`${style.formTitle}`}>Choose your gender</h3>
         <div className={`${style.genders}`}>
-          <div className={`${style.genderBox} ${style.activeBox}`}>
-            <FontAwesomeIcon icon={faCircleCheck} className={`${style.icon}`} />
+          <div id="head" onClick={handleToggle} className={`${style.genderBox}`}>
             <FontAwesomeIcon icon={faMars} className={`${style.genderIcon}`} />
             <h3 className={`${style.genderBoxTitle}`}>Male</h3>
           </div>
-          <div className={`${style.genderBox}`}>
-            <FontAwesomeIcon icon={faCircleCheck} className={`${style.icon}`} />
+          <div id="head" onClick={handleToggle} className={`${style.genderBox}`}>
+
             <FontAwesomeIcon icon={faVenus} className={`${style.genderIcon}`} />
             <h3 className={`${style.genderBoxTitle}`}>Female</h3>
           </div>
